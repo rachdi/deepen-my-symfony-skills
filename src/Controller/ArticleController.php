9f3a304e96a632460e6a2c511386d4c76d8f7ad6
Rachdi <bassem@tataragne.com>
2018-12-05 11:14:02 +0100

    adding twig

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -10,9 +10,10 @@ namespace App\Controller;
 
 
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 
-class ArticleController
+class ArticleController extends AbstractController
 {
     /**
      * @Route("/")
@@ -27,7 +28,14 @@ class ArticleController
      */
     public function show($slug)
     {
-        return new Response(sprintf("Future page to show the article %s",
-            $slug));
+        $comments = [
+            'I ate a normal rock once. It did NOT taste like bacon!',
+            'Woohoo! I\'m going on an all-asteroid diet!',
+            'I like bacon too! Buy some from my site! bakinsomebacon.com',
+        ];
+       return $this->render('article/show.html.twig', [
+          'title' => ucwords(str_replace('-', ' ', $slug)),
+           'comments' => $comments,
+       ]);
     }
 }
